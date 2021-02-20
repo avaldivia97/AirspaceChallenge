3eaf035a39658797d38e91adc7fd93076abb9a41
avaldivia97 <avaldivia97@outlook.com>
2021-02-20 16:19:41 -0500

    Added script for starting cypress, and wrote test case for valid login

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,29 @@
-describe('My First Test', () => {
-    it('Does not do much', () =>{
+describe('My First Test', ()=>{
+    it('Checks if cypress is up and running', ()=>{
         expect(true).to.equal(true)
+    })
+})
+
+describe('Visit Link', () => {
+    it('Visits my app', () =>{
+        cy.visit('https://the-internet.herokuapp.com/login')
+        cy.contains("Login Page")
+    })
+})
+
+describe('Check for username and password inputs', ()=>{
+    it('Checks for the  words username and password', ()=>{
+        cy.contains('Username')
+        cy.contains('Password')
+    })
+})
+
+describe('Checks valid login', ()=>{
+    it("types 'tomsmith' for the username and 'SuperSecretPassword!' for the password", ()=>{
+        cy.get('input:first').type('tomsmith')
+        cy.get('input:last').type('SuperSecretPassword!')
+        cy.get('form').submit()
+        cy.url().should('include', '/secure')
+        cy.get('a[href*="/logout"]').click()
     })
 })
