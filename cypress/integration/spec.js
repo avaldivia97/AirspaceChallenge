2b2b9d3f354c8b4b14634bd98b9bc6cd22f4d00c
avaldivia97 <avaldivia97@outlook.com>
2021-02-22 16:25:07 -0500

    Added one more test, see if the enter button works instead of click

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -15,11 +15,25 @@ describe('Log In Page Regression Suite', ()=>{
         cy.get('a[href*="/logout"]').click()
     })
 
+    it("Tests that the enter button can be used to login", ()=>{
+        cy.get('@username').type('tomsmith')
+        cy.get('@password').type('SuperSecretPassword!').type('{enter}')
+        cy.url().should('include', '/secure')
+        cy.get('a[href*="/logout"]').click()
+    })
+
     it("Tests log in with invalid login credentials", ()=>{
         cy.get('@username').type('testsmith')
         cy.get('@password').type('testpassword')
         cy.get('@submitButton').click()
         cy.contains('Your username is invalid!').should('be.visible')
+    })
+
+    it("Tests log in with valid username credentials and invalid password credentials", ()=>{
+        cy.get('@username').type('tomsmith')
+        cy.get('@password').type('testpassword')
+        cy.get('@submitButton').click()
+        cy.contains('Your password is invalid!').should('be.visible')
     })
 
     it("Tests log in with no credentials", ()=>{
